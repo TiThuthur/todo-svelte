7e4ce90559c038da7c5f2989feaa61c3ce6e1577
TiThuthur <thurthur.petit.py@gmail.com>
2026-03-03 08:28:37 +0100

    realisation de la liste

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	let todos = $state([]);
+	let newTodo = $state('');
+	const addTodo = () => {
+		const todo = {
+			id: todos.length + 1,
+			text: newTodo,
+			completed: false
+		};
+		todos = [...todos, todo];
+		newTodo = '';
+	};
+	const removeTodo = (id) => {
+		todos = todos.filter((todo) => todo.id !== id);
+	};
+</script>
+
+<section class="flex h-screen items-center justify-center p-4">
+	<div class="flex h-6/12 w-xl flex-col items-center rounded-lg bg-cyan-100 p-4 shadow-lg">
+		<h1>My todo app</h1>
+		<p>Bienvenue sur ta todo app!</p>
+		<div class="">
+			<p>Voici la liste de tes tâches :</p>
+			<ul>
+				{#each todos as todo}
+					<li>
+						{todo.text} <button onclick={() => removeTodo(todo.id)}>Supprimer</button>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
+</section>
